1f33d58ee92a6ee997f106fd87c4542bfd605594
rRedq <redq_000@mail.ru>
2024-10-07 19:29:00 +0500

    feat: add stream method solutions

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream } from 'fs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+  const rootPath = join(__dirname, 'files', 'fileToRead.txt');
+
+  const stream = createReadStream(rootPath);
+  stream.on('data', (data) => {
+    process.stdout.write(data);
+  });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import { createWriteStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+  const rootPath = join(__dirname, 'files', 'fileToWrite.txt');
+  const stream = createWriteStream(rootPath);
+
+  process.stdout.write('Please, type text:\n');
+
+  process.stdin.on('data', (data) => {
+    stream.write(data);
+  });
 };
 
 await write();
