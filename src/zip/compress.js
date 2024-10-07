da14636ae3feef39e762bfd2ccfc0cdffe02d326
rRedq <redq_000@mail.ru>
2024-10-07 19:29:32 +0500

    feat: add zip methods

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { join } from 'path';
+import { createGzip } from 'zlib';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const compress = async () => {
-    // Write your code here 
+  const compressPath = join(__dirname, 'files', 'fileToCompress.txt');
+  const zipPath = join(__dirname, 'files', 'archive.gz');
+
+  const readStream = createReadStream(compressPath);
+  const writeStream = createWriteStream(zipPath);
+
+  readStream.pipe(createGzip()).pipe(writeStream);
 };
 
 await compress();
