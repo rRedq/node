825f59fde24ec23bc25b94c7e9d46f7aa45bf325
rRedq <redq_000@mail.ru>
2024-10-07 19:38:24 +0500

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { join } from 'path';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+  const path = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const hash = createHash('sha256');
+  const readStream = createReadStream(path);
+
+  readStream.on('readable', () => {
+    const data = readStream.read();
+    if (data) hash.update(data);
+    else {
+      console.log(hash.digest('hex'));
+    }
+  });
 };
 
 await calculateHash();
