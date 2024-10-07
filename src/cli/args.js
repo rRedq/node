29c0c16f5de32bbb4ac5af9cc4a5f2579b5c2951
rRedq <redq_000@mail.ru>
2024-10-07 19:38:03 +0500

    feat: add cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+  const result = [];
+
+  for (let i = 0; i < argv.length; i++) {
+    if (argv[i].match(/--/)) {
+      result.push(`${argv[i].slice(2)} is ${argv[i + 1]}`);
+      i++;
+    }
+  }
+
+  console.log(result.join(', '));
 };
 
 parseArgs();
